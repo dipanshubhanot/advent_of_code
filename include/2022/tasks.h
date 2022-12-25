21dd5f2170b7d29443d1f168d3cca0acc4a36eee
Deepanshu Bhanot <dipanshubhanot@gmail.com>
2022-12-25 23:15:20 +1100

    Completed day1. Added cmake.

---
+++ include/2022/tasks.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <assert.h>
+#include "../utils.h"
+
+/// @brief  Prints the answer to task 1 from stdin
+void day1(std::string &filename);
---
+++ src/2022/main.cpp
@@ -0,0 +1,8 @@
+#include "2022/tasks.h"
+
+int main()
+{
+    auto fname = std::string("/Users/deepanshubhanot/workspace/advent_of_code/inputs/2022/task1.txt");
+    day1(fname);
+    return 0;
+}
