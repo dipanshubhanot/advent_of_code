f99f9a18a34dbc8576238d987fa1d36382e8ba46
Deepanshu Bhanot <dipanshubhanot@gmail.com>
2022-12-26 00:36:14 +1100

    Finished task 2

--- include/2022/tasks.h
+++ include/2022/tasks.h
@@ -4,5 +4,10 @@
 #include <assert.h>
 #include "../utils.h"
 
-/// @brief  Prints the answer to task 1 from stdin
+/// @brief  Prints the answer to task 1
+/// @param filename name of the input file
 void day1(std::string &filename);
+
+/// @brief Prints the answer to task 2 on std::out
+/// @param filename name of the input file
+void day2(std::string &filename);
